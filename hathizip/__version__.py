ed345f6b1fb132dd344fcbe3bfc21f0545686234
hborcher <hborcher@illinois.edu>
2018-02-20 17:11:05 -0600

    no longer needed

--- hathizip/__version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = 'HathiZip'
-__version__ = '0.1.0'
-__url__ = 'https://github.com/UIUCLibrary/HathiZip'
-__author__ = 'University of Illinois at Urbana Champaign'
-__author_email__ = 'hborcher@illinois.edu'
-__description__ = 'Creates .zip file packages for HathiTrust. Replacement script for HathiTrust Zip and Submit.'
-FULL_TITLE = "DS HathiTrust Zip for Submit"
