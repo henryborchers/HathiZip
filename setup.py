394792cdd215b89f3b8f0849c7dd362bfca0a460
Borchers, Henry Samuel <hborcher@illinois.edu>
2018-03-05 18:32:52 -0600

    added setuptools>=30.3.0 to install requirements

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     test_suite="tests",
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
+    install_requires=["setuptools>=30.3.0"],
     entry_points={
         "console_scripts": [
             "hathizip = hathizip.__main__:main"
