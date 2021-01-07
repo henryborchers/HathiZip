800130644f403ad9ca47cf7f83b7cf74700e23e5
henry <henryborchers@yahoo.com>
2021-01-07 14:36:24 -0600

    testPkg returns a closure

--- ci/jenkins/scripts/packaging.groovy
+++ ci/jenkins/scripts/packaging.groovy
@@ -14,43 +14,45 @@ def getToxEnv(args){
     }
 }
 
-def getAgent(agent){
-    def nodeLabel = getNodeLabel(agent)
-    return node(nodeLabel)
+def getAgent(args){
+    def nodeLabel = getNodeLabel(args.agent)
+    return { inner ->
+        node(nodeLabel){
+            ws{
+                checkout scm
+                def dockerImage
+//                 def dockerImageName = "dummy"
+                def dockerImageName = "dummy_${getToxEnv(args)}"
+                echo "Getting docker build args"
+                def dockerBuildArg = "-f ${args.agent.dockerfile.filename} ${args.agent.dockerfile.additionalBuildArgs} ."
+        //         TODO: change the docker image name
+                echo "Running docker with the following args ${dockerBuildArg}"
+                lock("docker build-${env.NODE_NAME}"){
+                    dockerImage = docker.build(dockerImageName, dockerBuildArg)
+                }
+                dockerImage.inside{
+                    inner()
+                }
+            }
+        }
+    }
 }
 
-// TODO: Make get agent which returns a clousure
 def testPkg(args = [:]){
-//     def agentRunner = getAgent(args.agent)
-    def nodeLabel = getNodeLabel(args.agent)
-//     agentRunner{
-    node(nodeLabel){
+    def agentRunner = getAgent(args)
+    agentRunner {
         checkout scm
-        def dockerImage
-        def dockerImageName = "dummy_${getToxEnv(args)}"
-//         TODO: change the docker image name
-        lock("docker build-${env.NODE_NAME}"){
-            dockerImage = docker.build(dockerImageName, "-f ${args.agent.dockerfile.filename} ${args.agent.dockerfile.additionalBuildArgs} .")
-        }
-        ws{
-            checkout scm
-            dockerImage.inside{
-                unstash "${args.stash}"
-                findFiles(glob: args.glob).each{
-                    echo "testing ${it.path}"
-
-                    def toxCommand = "tox --installpkg ${it.path} -e ${getToxEnv(args)}"
-                    if(isUnix()){
-                        sh(label: "Testing tox version", script: "tox --version")
-                        toxCommand = toxCommand + " --workdir /tmp/tox"
-
-                        sh(label: "Running Tox", script: toxCommand)
-                    } else{
-                        bat(label: "Testing tox version", script: "tox --version")
-                        toxCommand = toxCommand + " --workdir %TEMP%\\tox"
-                        bat(label: "Running Tox", script: toxCommand)
-                    }
-                }
+        unstash "${args.stash}"
+        findFiles(glob: args.glob).each{
+            def toxCommand = "tox --installpkg ${it.path} -e ${getToxEnv(args)}"
+            if(isUnix()){
+                sh(label: "Testing tox version", script: "tox --version")
+                toxCommand = toxCommand + " --workdir /tmp/tox"
+                sh(label: "Running Tox", script: toxCommand)
+            } else{
+                bat(label: "Testing tox version", script: "tox --version")
+                toxCommand = toxCommand + " --workdir %TEMP%\\tox"
+                bat(label: "Running Tox", script: toxCommand)
             }
         }
     }
