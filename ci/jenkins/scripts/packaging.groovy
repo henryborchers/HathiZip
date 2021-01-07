4982a62a15481aa5273759306271d93585baaf65
henry <henryborchers@yahoo.com>
2021-01-07 16:25:54 -0600

    Add setup and teardown optional args

--- ci/jenkins/scripts/packaging.groovy
+++ ci/jenkins/scripts/packaging.groovy
@@ -50,23 +50,31 @@ def getAgent(args){
 }
 
 def testPkg(args = [:]){
-//     TOOD: check if tox commmand is specified else defult to tox
     def tox = args['toxExec'] ? args['toxExec']: "tox"
-    def agentRunner = getAgent(args)
-    agentRunner {
+    def setup = args['testSetup'] ? args['testSetup']: {
         checkout scm
         unstash "${args.stash}"
-        findFiles(glob: args.glob).each{
-            def toxCommand = "${tox} --installpkg ${it.path} -e ${getToxEnv(args)}"
-            if(isUnix()){
-                sh(label: "Testing tox version", script: "${tox} --version")
-                toxCommand = toxCommand + " --workdir /tmp/tox"
-                sh(label: "Running Tox", script: toxCommand)
-            } else{
-                bat(label: "Testing tox version", script: "${tox} --version")
-                toxCommand = toxCommand + " --workdir %TEMP%\\tox"
-                bat(label: "Running Tox", script: toxCommand)
+    }
+    def teardown =  args['testTeardown'] ? args['testTeardown']: {}
+
+    def agentRunner = getAgent(args)
+    agentRunner {
+        setup()
+        try{
+            findFiles(glob: args.glob).each{
+                def toxCommand = "${tox} --installpkg ${it.path} -e ${getToxEnv(args)}"
+                if(isUnix()){
+                    sh(label: "Testing tox version", script: "${tox} --version")
+                    toxCommand = toxCommand + " --workdir /tmp/tox"
+                    sh(label: "Running Tox", script: toxCommand)
+                } else{
+                    bat(label: "Testing tox version", script: "${tox} --version")
+                    toxCommand = toxCommand + " --workdir %TEMP%\\tox"
+                    bat(label: "Running Tox", script: toxCommand)
+                }
             }
+        } finally{
+            teardown()
         }
     }
 }
