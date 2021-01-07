5cc4971e0a6b03ab64b1ff3c1568e11eb594cb89
henry <henryborchers@yahoo.com>
2021-01-07 11:24:56 -0600

    tox workdir in temp

--- ci/jenkins/scripts/packaging.groovy
+++ ci/jenkins/scripts/packaging.groovy
@@ -14,15 +14,23 @@ def getToxEnv(args){
     }
 }
 
+def getAgent(agent){
+    def nodeLabel = getNodeLabel(agent)
+    return node(nodeLabel)
+}
+
 // TODO: Make get agent which returns a clousure
 def test_pkg(args = [:]){
-    echo "args ${args}"
+//     def agentRunner = getAgent(args.agent)
     def nodeLabel = getNodeLabel(args.agent)
+//     agentRunner{
     node(nodeLabel){
         checkout scm
         def dockerImage
+        def dockerImageName = "dummy_${getToxEnv(args)}"
+//         TODO: change the docker image name
         lock("docker build-${env.NODE_NAME}"){
-            dockerImage = docker.build("dummy", "-f ${args.agent.dockerfile.filename} ${args.agent.dockerfile.additionalBuildArgs} .")
+            dockerImage = docker.build(dockerImageName, "-f ${args.agent.dockerfile.filename} ${args.agent.dockerfile.additionalBuildArgs} .")
         }
         ws{
             checkout scm
@@ -34,9 +42,11 @@ def test_pkg(args = [:]){
                     def toxCommand = "tox --installpkg ${it.path} -e ${getToxEnv(args)}"
                     if(isUnix()){
                         sh "tox --version"
+                        toxCommand = toxCommand + " --workdir /tmp/tox"
                         sh toxCommand
                     } else{
                         bat "tox --version"
+                        toxCommand = toxCommand + " --workdir %TEMP%\\tox"
                         bat toxCommand
                     }
                 }
